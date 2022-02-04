80e013fee0eb8b5b7baa5695f4a99e67a71440b3
Massimo Dong <dongmassimo@gmail.com>
2022-02-04 19:24:15 +0800

    add php

--- gen_files.sh
+++ gen_files.sh
@@ -1,2 +1,3 @@
-protoc --cpp_out=. wjudger.proto
-protoc --grpc_out=. --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` wjudger.proto
+protoc --cpp_out=./cc wjudger.proto
+protoc --grpc_out=./cc --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` wjudger.proto
+protoc --php_out=./php --grpc_out=./php --plugin=protoc-gen-grpc=`which grpc_php_plugin` wjudger.proto
