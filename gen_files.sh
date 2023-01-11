ec65c520faff711a5b8935edf8feca829d83372b
Massimo Dong <dongmassimo@gmail.com>
2023-01-11 09:37:29 +0800

    implement judger details

--- gen_files.sh
+++ gen_files.sh
@@ -1,3 +1,5 @@
+rm -R cc/*
+rm -R php/*
 protoc --cpp_out=./cc wjudger.proto
 protoc --grpc_out=./cc --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` wjudger.proto
 protoc --php_out=./php --grpc_out=./php --plugin=protoc-gen-grpc=`which grpc_php_plugin` wjudger.proto
